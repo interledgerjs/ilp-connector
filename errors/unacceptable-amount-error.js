bdf6fe6c9dfda4c68840d46d970b45dbbd451f1e
sentientwaffle <sentientwaffle@gmail.com>
2016-01-19 14:19:20 -0800

    [TASK] code review refactoring

--- errors/unacceptable-amount-error.js
+++ errors/unacceptable-amount-error.js
@@ -3,19 +3,15 @@
 const UnprocessableEntityError =
 require('five-bells-shared/errors/unprocessable-entity-error')
 
-module.exports = function UnacceptableAmountError (message) {
-  Error.captureStackTrace(this, this.constructor)
-  this.name = this.constructor.name
-  this.message = message
-}
-
-require('util').inherits(module.exports, UnprocessableEntityError)
-
-module.exports.prototype.handler = function *(ctx, log) {
-  log.warn('Unacceptable Amount: ' + this.message)
-  ctx.status = 422
-  ctx.body = {
-    id: this.name,
-    message: this.message
+class UnacceptableAmountError extends UnprocessableEntityError {
+  * handler (ctx, log) {
+    log.warn('Unacceptable Amount: ' + this.message)
+    ctx.status = 422
+    ctx.body = {
+      id: this.name,
+      message: this.message
+    }
   }
 }
+
+module.exports = UnacceptableAmountError
