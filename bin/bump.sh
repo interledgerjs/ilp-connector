293fda1bda71afaec5cb5a8dfd58437db399bc78
Alan Cohen <alan@ripple.com>
2016-03-02 10:43:28 -0800

    Add changesSinceCurrentVersion
    Adds a sanity check for checking the commits since the last released version

--- bin/bump.sh
+++ bin/bump.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# http://www.tldp.org/LDP/abs/html/options.html
 set -eo pipefail
 
 # http://stackoverflow.com/questions/1593051/how-to-programmatically-determine-the-current-checked-out-git-branch
@@ -6,7 +8,7 @@ checkOnMaster() {
   local branch_name
   branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
   if [ "${branch_name}" = "refs/heads/master" ]; then
-    echo "On master. Good to go."
+    printf "On master. Good to go.\n\n"
   else
     echo "Need to be on master to proceed."
     exit 1
@@ -25,17 +27,28 @@ syncAndCheckWithRemote() {
   base="$(git merge-base @ "@{u}")"
 
   if [ "$mine" = "$remote" ]; then
-    echo "Local branch up to date with master. Proceeding.."
+    printf "Local branch up to date with master. Proceeding..\n\n"
   elif [ "$mine" = "$base" ]; then
-    echo "Local not up to date. Need to pull."
-    read -rsp $'Press any key to continue... or CTRL+C to exit\n' -n1
+    printf "Local not up to date. Need to pull.\n\n"
+    read -rsp $'Press any key to continue... or CTRL+C to exit\n\n' -n1
   elif [ "$remote" = "$base" ]; then
-    echo "Local is ahead of remote. Need to push."
+    printf "Local is ahead of remote. Need to push.\n\n"
     exit 1
   else
-    echo "Local diverged from remote"
+    printf "Local diverged from remote\n\n"
     exit 1
   fi
+}
+
+checkChangesSinceCurrentVersion() {
+  local currentVersion
+  local changes
+  currentVersion=$(npm ls --depth=-1 2>/dev/null | head -1 | cut -f 1 -d " " | sed 's/.*@//')
+  changes=$(git --no-pager log v"$currentVersion".. --oneline --no-merges --reverse)
+
+  printf "These are the changes since the last version\n\n"
+  printf "%s\n\n" "$changes"
+  read -rsp $'Press any key to continue... or CTRL+C to exit\n\n' -n1
 }
 
 unitTest() {
@@ -63,13 +76,14 @@ versionCommitAndTag() {
 }
 
 pushCommitandTags() {
-  echo "Pushing commit and tags..."
+  printf "Pushing commit and tags...\n\n"
   git push --follow-tags
 }
 
 main() {
   checkOnMaster
   syncAndCheckWithRemote
+  checkChangesSinceCurrentVersion
   unitTest
   versionCommitAndTag "$1"
   pushCommitandTags
