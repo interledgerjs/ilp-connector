8c907ffa6fa4c414b4570d81117c3714002004ec
Alan Cohen <alan@ripple.com>
2016-03-21 14:01:12 -0700

    Create payments domain model
    Remove services/payments and services/subscriptions
    Use a functional interface
    
    This is part of refactoring to a three-layer architecture

---
+++ src/models/subscriptions.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const _ = require('lodash')
+const log = require('../services/log')('subscriptions')
+const payments = require('../models/payments')
+
+function * subscribePairs (pairs, ledgersService, config) {
+  let ledgers = _(pairs)
+    .flatten()
+    .map(function (d) {
+      return d.split('@').slice(1).join('@')
+    })
+    .uniq()
+    .value()
+
+  // Subscribe to all ledgers in parallel.
+  yield ledgers.map((l) => subscribeLedger(l, ledgersService, config))
+}
+
+function * subscribeLedger (ledger, ledgersService, config) {
+  log.info('subscribing to ' + ledger)
+  yield ledgersService.subscribe(ledger, {
+    uri: config.getIn(['server', 'base_uri']) + '/notifications',
+    transfer: payments.updateTransfer
+  })
+}
+
+module.exports = {
+  subscribePairs,
+  subscribeLedger
+}
--- src/services/subscriber.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const Subscriber = require('../lib/subscriber').Subscriber
-const payments = require('./payments')
-const config = require('./config')
-
-module.exports = new Subscriber(config, payments)
