7eb7a1c67cf069f0c1afdee375be96a46420408e
Alan Cohen <alan@ripple.com>
2016-03-28 09:47:57 -0700

    Cleanup: Remove unused files

--- test/services/log.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const hub = require('mag-hub')
-const mag = require('mag')
-const log = require('five-bells-shared/lib/log')
-
-module.exports = log(mag, hub)
--- test/services/subscriber.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const Subscriber = require('../lib/subscriber').Subscriber
-const config = require('./config')
-
-module.exports = new Subscriber(config)
