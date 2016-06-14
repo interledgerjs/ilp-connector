aab7dfb46274afd4231c73bb54d72b85bceb6314
CircleCI <info@circleci.com>
2016-06-14 17:35:37 +0000

    chore: update gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "five-bells-connector",
-  "version": "7.0.1",
+  "version": "7.0.2",
   "description": "Five Bells connector reference implementation",
   "title": "Five Bells Connector API",
   "url": "https://acme-connector.example",
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-06-14T17:34:52.529Z",
+    "time": "2016-06-14T17:35:36.907Z",
     "url": "http://apidocjs.com",
     "version": "0.13.2"
   }
