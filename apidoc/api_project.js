1403cf8a082b9a4d69a0cb1557e90e30d5d3cc73
CircleCI <info@circleci.com>
2016-09-30 18:32:00 +0000

    chore: update gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "ilp-connector",
-  "version": "10.1.0",
+  "version": "10.1.1",
   "description": "ILP connector reference implementation",
   "title": "ILP Connector API",
   "url": "https://acme-connector.example",
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-09-28T08:12:13.603Z",
+    "time": "2016-09-30T18:31:59.046Z",
     "url": "http://apidocjs.com",
     "version": "0.13.2"
   }
