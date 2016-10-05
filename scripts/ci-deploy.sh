a8e75b0f1c9d5b81ae630d329e9e011b1dcab044
Ben Sharafian <benms64@gmail.com>
2016-10-04 17:12:11 -0700

    fix: fix the deploy script

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -3,7 +3,7 @@
 publishNpm() {
   # Push NPM package if not yet published
   mv npmrc-env .npmrc
-  if [ -z "$(npm info $(npm ls --depth=-1 2>/dev/null | head -1 | cut -f 1 -d " ") 2>/dev/null)" ]; then
+  if [ "$(npm info $(npm ls --depth=-1 2>/dev/null | head -1 | cut -f 1 -d " ") 2>/dev/null)" = "undefined" ]; then
     npm publish
   fi
 }
