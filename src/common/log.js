562e2e1c5862b9e90d2b23c359483648a9805792
Stefan Thomas <justmoon@members.fsf.org>
2016-10-22 15:02:48 -0700

    refactor: switch to riverpig logging

--- src/common/log.js
+++ src/common/log.js
@@ -1,44 +1,13 @@
 'use strict'
 
-const bunyan = require('bunyan')
+const riverpig = require('riverpig')
 
 const Config = require('five-bells-shared').Config
 const envPrefix = 'CONNECTOR'
 const logLevel = Config.getEnv(envPrefix, 'LOG_LEVEL')
 
-function createLogger (name) {
-  const logger = bunyan.createLogger({
-    name: name,
-    level: logLevel,
-    stream: process.stdout
-  })
-  return logger
-}
+const defaultLogger = riverpig('connector')
 
-const defaultLogger = createLogger('connector')
-const loggers = [defaultLogger]
+defaultLogger.create = riverpig
 
-function createChildLogger (module) {
-  const logger = defaultLogger.child({
-    module: module
-  })
-  loggers.push(logger)
-  return logger
-}
-
-// For unit testing
-function setOutputStream (outputStream) {
-  loggers.forEach((logger) => {
-    logger.streams = []
-    logger.addStream({
-      type: 'stream',
-      stream: outputStream,
-      level: logLevel
-    })
-    logger.currentOutput = outputStream
-  })
-}
-
-defaultLogger.create = createChildLogger
-defaultLogger.setOutputStream = setOutputStream
 module.exports = defaultLogger
