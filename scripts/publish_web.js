b7b0a1d93807d5fa072d1b4c7a2b08115d273f8b
sentientwaffle <sentientwaffle@gmail.com>
2016-10-28 09:17:33 -0700

    fix: remove apidoc

--- scripts/publish_web.js
+++ scripts/publish_web.js
@@ -12,17 +12,6 @@ console.log('\n# Cloning web branch')
 exec('rm -rf web', { cwd })
 exec('git clone git@github.com:interledgerjs/ilp-connector.git --branch gh-pages --single-branch web', { cwd })
 
-// Update apidoc
-console.log('\n# Updating API docs')
-exec('npm run apidoc', { cwd })
-exec('mkdir -p web/apidoc', { cwd })
-exec('cp -r apidoc-out/* web/apidoc/', { cwd })
-
-// Update apidoc-template
-console.log('\n# Updating API doc template')
-exec('wget https://github.com/interledger/apidoc-template/archive/master.tar.gz -O - | tar xzf - --strip 1 -C web/apidoc', { cwd })
-exec('rm web/apidoc/.gitignore')
-
 // Push changes
 console.log('\n# Pushing web branch')
 cwd = path.resolve(cwd, 'web')
