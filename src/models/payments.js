2cea5f817caa413c0c95883c26be3a1bdfe63f35
sentientwaffle <sentientwaffle@gmail.com>
2017-02-17 08:30:27 -0800

    feat: BREAKING Use RejectionMessage for ILP Errors

--- src/models/payments.js
+++ src/models/payments.js
@@ -5,6 +5,7 @@ const log = require('../common').log.create('payments')
 const executeSourceTransfer = require('../lib/executeSourceTransfer')
 const validator = require('../lib/validate')
 const startsWith = require('lodash/startsWith')
+const IlpError = require('../errors/ilp-error')
 
 function validateIlpHeader (sourceTransfer) {
   validator.validate('IlpHeader', sourceTransfer.data.ilp_header)
@@ -23,14 +24,38 @@ function * settle (sourceTransfer, destinationTransfer, config, ledgers) {
   log.debug('Settle payment, destination: ' + JSON.stringify(destinationTransfer))
   yield ledgers.getPlugin(destinationTransfer.ledger)
     .sendTransfer(destinationTransfer)
-    .catch((err) =>
-      ledgers.getPlugin(sourceTransfer.ledger)
-        .rejectIncomingTransfer(sourceTransfer.id, 'destination transfer failed: ' + err.message)
-        .then(() => { throw err }))
+    .catch((err) => {
+      const rejectionMessage =
+        (err.name === 'InvalidFieldsError' || err.name === 'DuplicateIdError') ? {
+          code: 'S00',
+          name: 'Bad Request',
+          message: 'destination transfer failed: ' + err.message
+        } : {
+          code: 'T01',
+          name: 'Ledger Unreachable',
+          message: 'destination transfer failed: ' + err.message
+        }
+      return ledgers.getPlugin(sourceTransfer.ledger)
+        .rejectIncomingTransfer(sourceTransfer.id, Object.assign({
+          triggered_by: ledgers.getPlugin(destinationTransfer.ledger).getAccount(),
+          triggered_at: (new Date()).toISOString(),
+          additional_info: {}
+        }, rejectionMessage))
+        .then(() => { throw err })
+    })
 }
 
 function * updateIncomingTransfer (sourceTransfer, ledgers, config, routeBuilder) {
-  validateIlpHeader(sourceTransfer)
+  try {
+    validateIlpHeader(sourceTransfer)
+  } catch (err) {
+    yield rejectIncomingTransfer(sourceTransfer, {
+      code: 'S01',
+      name: 'Invalid Packet',
+      message: err.message
+    }, ledgers)
+    return
+  }
 
   const destinationAddress = sourceTransfer.data.ilp_header.account
   const myAddress = ledgers.getPlugin(sourceTransfer.ledger).getAccount()
@@ -40,14 +65,24 @@ function * updateIncomingTransfer (sourceTransfer, ledgers, config, routeBuilder
       destinationAddress,
       myAddress
     )
-    yield rejectIncomingTransfer(sourceTransfer, 'transfer addressed to me', ledgers)
+    yield rejectIncomingTransfer(sourceTransfer, {
+      code: 'S06',
+      name: 'Unexpected Payment',
+      message: 'cannot process transfer addressed to destination which starts with my address'
+    }, ledgers)
 
     return
   }
 
-  const destinationTransfer = yield routeBuilder.getDestinationTransfer(sourceTransfer)
-
-  yield validateExpiry(sourceTransfer, destinationTransfer, config)
+  let destinationTransfer
+  try {
+    destinationTransfer = yield routeBuilder.getDestinationTransfer(sourceTransfer)
+    yield validateExpiry(sourceTransfer, destinationTransfer, config)
+  } catch (err) {
+    if (!(err instanceof IlpError)) throw err
+    yield rejectIncomingTransfer(sourceTransfer, err.rejectionMessage, ledgers)
+    return
+  }
   yield settle(sourceTransfer, destinationTransfer, config, ledgers)
 }
 
@@ -61,19 +96,26 @@ function * processExecutionFulfillment (transfer, fulfillment, ledgers, backend)
   }
 }
 
-function * rejectIncomingTransfer (sourceTransfer, rejectionMessage, ledgers) {
+function * rejectIncomingTransfer (sourceTransfer, _rejectionMessage, ledgers) {
+  const myAddress = ledgers.getPlugin(sourceTransfer.ledger).getAccount()
+  const rejectionMessage = Object.assign({
+    triggered_by: myAddress,
+    triggered_at: (new Date()).toISOString(),
+    additional_info: {}
+  }, _rejectionMessage)
   if (sourceTransfer.executionCondition) {
     log.debug(
       'rejecting incoming transfer id=%s reason=%s',
       sourceTransfer.id,
-      rejectionMessage
+      JSON.stringify(rejectionMessage)
     )
-    yield ledgers.getPlugin(sourceTransfer.ledger).rejectIncomingTransfer(sourceTransfer.id, 'transfer addressed to me')
+    yield ledgers.getPlugin(sourceTransfer.ledger)
+      .rejectIncomingTransfer(sourceTransfer.id, rejectionMessage)
   } else {
     log.debug(
       'ignoring incoming optimistic transfer id=%s reason=%s',
       sourceTransfer.id,
-      rejectionMessage
+      JSON.stringify(rejectionMessage)
     )
   }
 }
@@ -86,7 +128,9 @@ function * rejectSourceTransfer (destinationTransfer, rejectionMessage, ledgers)
   validator.validate('Uuid', sourceTransferId)
 
   yield ledgers.getPlugin(sourceTransferLedger)
-    .rejectIncomingTransfer(sourceTransferId, rejectionMessage)
+    .rejectIncomingTransfer(sourceTransferId, Object.assign(rejectionMessage, {
+      forwarded_by: ledgers.getPlugin(sourceTransferLedger).getAccount()
+    }))
     .catch(() => {
       log.warn('Attempted to reject source transfer but it was unsucessful')
     })
