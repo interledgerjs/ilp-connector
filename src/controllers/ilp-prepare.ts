b83cf238c87dd68a3d98d9ec330a82bafdf42095
Stefan Thomas <justmoon@members.fsf.org>
2018-02-12 07:34:19 -0800

    feat: add ping feature

--- src/controllers/ilp-prepare.ts
+++ src/controllers/ilp-prepare.ts
@@ -3,11 +3,11 @@ import { create as createLogger } from '../common/log'
 const log = createLogger('ilp-prepare')
 import reduct = require('reduct')
 
-import Config from '../services/config'
 import Accounts from '../services/accounts'
 import RouteBuilder from '../services/route-builder'
 import RateBackend from '../services/rate-backend'
 import PeerProtocolController from '../controllers/peer-protocol'
+import EchoController from '../controllers/echo'
 
 import UnreachableError from '../errors/unreachable-error'
 
@@ -16,18 +16,18 @@ const { sha256 } = require('../lib/utils')
 const PEER_PROTOCOL_PREFIX = 'peer.'
 
 export default class IlpPrepareController {
-  protected config: Config
-  protected accounts: Accounts
-  protected routeBuilder: RouteBuilder
-  protected backend: RateBackend
-  protected peerProtocolController: PeerProtocolController
+  private accounts: Accounts
+  private routeBuilder: RouteBuilder
+  private backend: RateBackend
+  private peerProtocolController: PeerProtocolController
+  private echoController: EchoController
 
   constructor (deps: reduct.Injector) {
-    this.config = deps(Config)
     this.accounts = deps(Accounts)
     this.routeBuilder = deps(RouteBuilder)
     this.backend = deps(RateBackend)
     this.peerProtocolController = deps(PeerProtocolController)
+    this.echoController = deps(EchoController)
   }
 
   async sendData (
@@ -42,6 +42,8 @@ export default class IlpPrepareController {
 
     if (destination.startsWith(PEER_PROTOCOL_PREFIX)) {
       return this.peerProtocolController.handle(packet, sourceAccount, { parsedPacket })
+    } else if (destination === this.accounts.getOwnAddress()) {
+      return this.echoController.handle(packet, sourceAccount, { parsedPacket, outbound })
     }
 
     const { nextHop, nextHopPacket } = await this.routeBuilder.getNextHopPacket(sourceAccount, parsedPacket)
