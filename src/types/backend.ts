e0a912c7492e46266025ed57220c7be608bab5f6
jeff <tangj1122@gmail.com>
2019-03-06 13:49:06 -0800

    feat: add backend.accounts and .submitPacket() (#479)

--- src/types/backend.ts
+++ src/types/backend.ts
@@ -1,15 +1,20 @@
+import Accounts from '../services/accounts'
 import { AccountInfo } from '../types/accounts'
+import * as IlpPacket from 'ilp-packet'
 
 export interface SubmitPaymentParams {
   sourceAccount: string
   destinationAccount: string
   sourceAmount: string
   destinationAmount: string
+  parsedPacket?: IlpPacket.IlpPrepare
+  result?: Buffer
 }
 
 /** API exposed by the connector to its backends */
 export interface BackendServices {
   getInfo: (accountId: string) => AccountInfo | undefined
+  accounts?: Accounts
 }
 
 export interface BackendConstructor {
@@ -20,4 +25,5 @@ export interface BackendInstance {
   connect (): Promise<void>
   getRate (sourceAccount: string, destinationAccount: string): Promise<number>
   submitPayment (params: SubmitPaymentParams): Promise<void>
+  submitPacket? (params: SubmitPaymentParams): Promise<void>
 }
