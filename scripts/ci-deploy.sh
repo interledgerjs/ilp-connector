68325681990f05c65eccaad258a08bdb84c585bd
sentientwaffle <sentientwaffle@gmail.com>
2019-06-28 07:41:05 -0700

    chore(circleci): remove old docker flag

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -15,7 +15,7 @@ pushDocker() {
   local REPO="js-ilp-connector"
   # rm is false because on Circle the process doesn't have permissions to delete the intermediate container
   docker build -t $NAMESPACE$REPO --rm=false .
-  docker login -u $DOCKER_USER -p $DOCKER_PASS -e $DOCKER_EMAIL $REGISTRY
+  docker login -u $DOCKER_USER -p $DOCKER_PASS $REGISTRY
   docker tag $NAMESPACE$REPO":latest" $NAMESPACE$REPO":$(git describe)"
   docker push $NAMESPACE$REPO":latest"
   docker push $NAMESPACE$REPO":$(git describe)"
