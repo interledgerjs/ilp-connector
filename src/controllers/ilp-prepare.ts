ed74d882d3fa2b2bc48532f731d94386a2c7ad26
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2020-09-21 17:33:49 -0400

    fix(echo): apply business rules
    - pong packets were forwarded without a cross-currency conversion to the destination asset
    - pong packets were forwarded without expiry checking and incorrect min message window deduction
    - rewrite echo as a middleware so business rules are applied
    - add tests for ping/pong packets
    - fix package vulnerabilities
    - remove coinmarketcap backend (api deprecated)
    - minor cleanup to README

--- src/controllers/ilp-prepare.ts
+++ src/controllers/ilp-prepare.ts
@@ -3,27 +3,21 @@ import { create as createLogger } from '../common/log'
 const log = createLogger('ilp-prepare')
 import reduct = require('reduct')
 
-import Accounts from '../services/accounts'
 import RouteBuilder from '../services/route-builder'
 import RateBackend from '../services/rate-backend'
 import PeerProtocolController from '../controllers/peer-protocol'
-import EchoController from '../controllers/echo'
 
 const PEER_PROTOCOL_PREFIX = 'peer.'
 
 export default class IlpPrepareController {
-  private accounts: Accounts
   private routeBuilder: RouteBuilder
   private backend: RateBackend
   private peerProtocolController: PeerProtocolController
-  private echoController: EchoController
 
   constructor (deps: reduct.Injector) {
-    this.accounts = deps(Accounts)
     this.routeBuilder = deps(RouteBuilder)
     this.backend = deps(RateBackend)
     this.peerProtocolController = deps(PeerProtocolController)
-    this.echoController = deps(EchoController)
   }
 
   async sendData (
@@ -38,8 +32,6 @@ export default class IlpPrepareController {
 
     if (destination.startsWith(PEER_PROTOCOL_PREFIX)) {
       return this.peerProtocolController.handle(packet, sourceAccount, { parsedPacket })
-    } else if (destination === this.accounts.getOwnAddress()) {
-      return this.echoController.handle(packet, sourceAccount, { parsedPacket, outbound })
     }
 
     const { nextHop, nextHopPacket } = await this.routeBuilder.getNextHopPacket(sourceAccount, parsedPacket)
